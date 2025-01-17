e2d9bb0e410ac0fc2b0ddd803ae8d33db0042d3e
Ahinez <nababkahvlad64@gmail.com>
2025-01-17 19:02:53 +0700

    changed the window resolution and made it not resizable. added text at the top of the program and added an input area. removed the exit button.

--- 8Ball (Tkinter)/tk8ball.py
+++ 8Ball (Tkinter)/tk8ball.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 def Exit():
@@ -7,16 +7,28 @@ def Exit():
 def Send():
     print('Ты нажал на кнопку "Send"')
 
-root = Tk()
+root = tk.Tk()
 
 root.title('8Ball')
+
 root.iconbitmap('E:\Projects\MyPyhtonProjects\8Ball (Tkinter)\8ball.ico')
-root.geometry('500x500+700+300')
 
-btn = ttk.Button(text='Send', command=Send)
-btn.pack(expand=True, anchor=SE, padx=10)
+root.geometry('800x500+600+300')
+root.resizable(False, False)
 
-btn = ttk.Button(text='Exit', command=Exit)
-btn.pack(anchor=SE, padx=10, pady=[0, 10])
+label = tk.Label(root, text="Ask your question", font=('Roboto Mono Light', 18))
+label.pack(padx=20, pady=20)
+
+textbox =tk.Text(root, height=1, font=('Roboto Mono Light', 16))
+textbox.pack(side='bottom', padx=[10, 100], pady=10)
+
+sendbtn = ttk.Button(root, text='Send', command=Send)
+sendbtn.place(x=710, y=458, height=32)
+
+# Send and Exit buttons
+# btn = ttk.Button(text='Send', command=Send)
+# btn.place(relx=0.88, rely=0.85) # btn.pack(expand=True, anchor=SE, padx=10)
+# btn = ttk.Button(text='Exit', command=Exit)
+# btn.place(relx=0.88, rely=0.90) # btn.pack(anchor=SE, padx=10, pady=[0, 10])
 
 root.mainloop()
