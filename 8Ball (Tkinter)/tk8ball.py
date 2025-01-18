80fe76f18167dd0c1591b4ea69361cacac6b1ad3
Ahinez <nababkahvlad64@gmail.com>
2025-01-18 14:58:50 +0700

    Removed submit button, text field. Added input area. When you write something in it, after you press “Enter” a message appears in the console about what you have entered.

--- 8Ball (Tkinter)/tk8ball.py
+++ 8Ball (Tkinter)/tk8ball.py
@@ -7,6 +7,9 @@ def Exit():
 def Send():
     print('Ты нажал на кнопку "Send"')
 
+def Output(event):
+    print(f'You entered: {entry.get()}')
+
 root = tk.Tk()
 
 root.title('8Ball')
@@ -19,11 +22,17 @@ root.resizable(False, False)
 label = tk.Label(root, text="Ask your question", font=('Roboto Mono Light', 18))
 label.pack(padx=20, pady=20)
 
-textbox =tk.Text(root, height=1, font=('Roboto Mono Light', 16))
-textbox.pack(side='bottom', padx=[10, 100], pady=10)
+entry = ttk.Entry(width=125)
+entry.pack(side='bottom', pady=10)
+entry.bind("<Return>", Output)
 
-sendbtn = ttk.Button(root, text='Send', command=Send)
-sendbtn.place(x=710, y=458, height=32)
+# Textbox
+# textbox =tk.Text(root, height=1, font=('Roboto Mono Light', 16))
+# textbox.pack(side='bottom', padx=[10, 100], pady=10)
+# textbox.bind("<Return>", Output)
+# Send button for text box
+# sendbtn = ttk.Button(root, text='Send', command=Send)
+# sendbtn.place(x=710, y=458, height=32)
 
 # Send and Exit buttons
 # btn = ttk.Button(text='Send', command=Send)
