5b445e1a5d3035d3c0b563b5c690f1d7cdd7e355
Ahinez <nababkahvlad64@gmail.com>
2025-01-20 13:26:39 +0700

    the final version of tk8ball. the programm was compiled by pyinstaller

--- 8Ball (Tkinter)/tk8ball.py
+++ 8Ball (Tkinter)/tk8ball.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
-
-def Exit():
-    root.destroy()
+from random import choice
 
 def Send():
-    print('Ты нажал на кнопку "Send"')
+    output['text'] = f'Your question is: {entry.get()}\nAnswer: {choice(answers)}'
 
 def Output(event):
-    output['text'] = f'You entered: {entry.get()}'
+    output['text'] = f'Your question is: {entry.get()}\nAnswer: {choice(answers)}'
+
+answers = ['Undeniably', 'Foregone', 'There\'s no doubt about it', 'Definitely yes', 'You can be sure of that', 'I think so', 'Most likely', 'Good prospects', \
+           'The signs say yes', 'Yes', 'It\'s not clear yet, try again', 'Ask me later', 'It\'s better not to tell', 'There\'s no way to predict right now', 'Concentrate and ask again', \
+            'Don\'t even think about it', 'My answer is no', 'Not according to my records', 'The outlook is not good', 'It\'s highly doubtful']
 
 root = tk.Tk()
 
@@ -26,21 +28,10 @@ output = tk.Label(relief='groove', font=('Roboto Mono Light', 14))
 output.pack(expand=1, padx=10, fill='both')
 
 entry = ttk.Entry()
-entry.pack(side='bottom', pady=10, fill='x', padx=10)
+entry.pack(side='bottom', pady=10, fill='x', padx=[10,95])
 entry.bind("<Return>", Output)
 
-# Textbox
-# textbox =tk.Text(root, height=1, font=('Roboto Mono Light', 16))
-# textbox.pack(side='bottom', padx=[10, 100], pady=10)
-# textbox.bind("<Return>", Output)
-# Send button for text box
-# sendbtn = ttk.Button(root, text='Send', command=Send)
-# sendbtn.place(x=710, y=458, height=32)
-
-# Send and Exit buttons
-# btn = ttk.Button(text='Send', command=Send)
-# btn.place(relx=0.88, rely=0.85) # btn.pack(expand=True, anchor=SE, padx=10)
-# btn = ttk.Button(text='Exit', command=Exit)
-# btn.place(relx=0.88, rely=0.90) # btn.pack(anchor=SE, padx=10, pady=[0, 10])
+btn = ttk.Button(text='Send', command=Send)
+btn.place(relx=0.89, rely=0.94)
 
 root.mainloop()
---
+++ 8Ball (Tkinter)/tk8ball.spec
@@ -0,0 +1,39 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['tk8ball.py'],
+    pathex=[],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+    optimize=0,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='tk8ball',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    icon=['E:\\Projects\\MyPyhtonProjects\\8Ball (Tkinter)\\8ball.ico'],
+)
